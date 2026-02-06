4e14d720e67d19c698a920cfa4e75fe8d68ec4a2
Kevin Arias <kev@Kevs-MBA-5.local>
2026-02-05 22:57:04 -0500

    printed values

--- nbar_lifetime.py
+++ nbar_lifetime.py
@@ -7,14 +7,19 @@ taus = np.append(tau_n, np.logspace(2, -9, 12)) # exponents are 2 all the way to
 
 beta = 0.7
 gamma = 1/(np.sqrt(1-beta**2)) # ~1.40028
+print(f'gamma = {gamma}')
+
 lambda_0 = 1/taus
 lambda_l = lambda_0/gamma # 13 values
+print(f'lambda lab for 887.6s lifetime = {lambda_l[0]}')
 
 l = 6 # [m]
 t = l/(beta * 3e08) 
 
 P_survive = np.exp(-lambda_l*t)
 P_decay = 1 - P_survive
+print(f'Probability of neutron decay for 887.6s lifetime = {P_decay[0]}')
+
 N_n = 1/P_decay
 for i, tau in enumerate(taus):
     print(f'Number of neutrons needed if tau_rest is {tau:9.3e} s: {N_n[i]:9.3e}')
